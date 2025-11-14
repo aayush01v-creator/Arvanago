f8ff860b9233d05720607c7b92312dd6b02c26bc
aayush01v-creator <aayush01v@gmail.com>
2025-11-14 23:47:33 +0530

    fix: resolve react router aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -15,7 +16,7 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        "@": "/src"
+        "@": fileURLToPath(new URL("./", import.meta.url))
       }
     },
     build: {
