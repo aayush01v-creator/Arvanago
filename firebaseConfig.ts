b0606fed4aaaf2114bf5cd4669b3a6b7b80c8728
Aayush <aayush01v@gmail.com>
2025-11-20 20:18:02 +0530

    Add Firebase configuration file

---
+++ firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRo2obm7A4Bd4lwReB165IzeMCOmdxfrQ",
+  authDomain: "firstfly-ea93b.firebaseapp.com",
+  projectId: "firstfly-ea93b",
+  storageBucket: "firstfly-ea93b.firebasestorage.app",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "1:994056761685:web:ca979db9fd4b4b6febbfdc",
+  measurementId: "G-5JRFM7YLD3"
+};
